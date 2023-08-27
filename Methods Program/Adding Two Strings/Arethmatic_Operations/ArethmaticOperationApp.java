422e241d0afb54c16bb11c16bd057712b9d36dbc
praveengowd004 <142770647+praveengowd004@users.noreply.github.com>
2023-08-27 21:06:37 +0530

    Add files via upload

---
+++ Methods Program/Adding Two Strings/Arethmatic_Operations/ArethmaticOperationApp.java
@@ -0,0 +1,21 @@
+package arethmatic_operations;
+
+public class ArethmaticOperationApp
+{
+	public static  int sub(int a,int b)
+	{
+		int res =a-b;
+		return (res);
+	}
+	
+	public static int add(int a, int b)
+	{
+		int res=a+b;
+		return (res);
+	}
+
+	public static int mul(int a ,int b)
+	{
+		return(a*b);
+	}
+}
