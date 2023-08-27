e58a469963cdc58548eff5ab1fb873a14a4612c1
praveengowd004 <142770647+praveengowd004@users.noreply.github.com>
2023-08-27 23:22:15 +0530

    Add files via upload

---
+++ Control_Constructs/Do While Loop/Printing_60_To_25.java
@@ -0,0 +1,16 @@
+package DoWhile;
+
+public class Printing_60_To_25 {
+
+	public static void main(String[] args) 
+	{
+		int i=60;
+		do
+		{
+			System.out.println(i);
+			i--;
+		}
+		while(i>=25);
+	}
+
+}
