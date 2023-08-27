f679857100d64690b2e0e2ea0c9684dfe9d8e232
praveengowd004 <142770647+praveengowd004@users.noreply.github.com>
2023-08-27 21:01:41 +0530

    Add files via upload

---
+++ Methods Program/Adding Two Strings/Area_Of_Rectangle/RectangleArea.java
@@ -0,0 +1,12 @@
+package area_of_rectangle;
+
+public class RectangleArea 
+{
+	//creating method
+	public static double CalculateRectangleArea(double a,double b)
+	{
+		double z=a*b;
+		//returning valve
+		return z;
+	}
+}
