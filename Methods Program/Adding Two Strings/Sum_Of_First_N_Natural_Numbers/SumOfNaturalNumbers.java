47efc0d731d59b73169e51ddc730d76254a4c06f
praveengowd004 <142770647+praveengowd004@users.noreply.github.com>
2023-08-28 09:10:43 +0530

    Add files via upload

---
+++ Methods Program/Adding Two Strings/Sum_Of_First_N_Natural_Numbers/SumOfNaturalNumbers.java
@@ -0,0 +1,19 @@
+package sum_of_first_N_natural_numbers;
+import java.util.Scanner;
+public class SumOfNaturalNumbers {
+
+	public static void main(String[] args) 
+	{
+		
+		Scanner scan=new Scanner(System.in);
+		//taking input from user
+		System.out.println("sum of n natural numbers");
+		System.out.println("enter n valve");
+		//storing valve into a variable
+		int a =scan.nextInt();
+		System.out.println("sum of first "+ a + " natural numbers");
+		SumOfNaturalNumbersApp.sum(a);
+
+	}
+
+}
