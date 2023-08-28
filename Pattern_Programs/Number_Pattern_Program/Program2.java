3bf49b697770c0759b066d121255bcce2cdbfea3
praveengowd004 <142770647+praveengowd004@users.noreply.github.com>
2023-08-28 11:19:09 +0530

    Add files via upload

---
+++ Pattern_Programs/Number_Pattern_Program/Program2.java
@@ -0,0 +1,19 @@
+package number_pattern_programs;
+
+public class Program2
+{
+
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=5;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
