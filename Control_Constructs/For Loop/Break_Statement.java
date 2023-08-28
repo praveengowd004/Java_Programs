ecac4b9595ee8476e1b3464fd2d6f9eae8210ecd
praveengowd004 <142770647+praveengowd004@users.noreply.github.com>
2023-08-28 09:00:58 +0530

    Add files via upload

---
+++ Control_Constructs/For Loop/Break_Statement.java
@@ -0,0 +1,19 @@
+package ForLoop;
+
+public class Break_Statement {
+
+	public static void main(String[] args) 
+	{
+		for (int i =1 ; i<=10 ;i++)
+		{
+			if (i==7)
+			{
+			//using break statement
+			break;
+			}
+		System.out.println(i);
+		}
+		
+	}
+
+}
