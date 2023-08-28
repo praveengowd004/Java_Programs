c0f3a9a934e8576dacbb15230837aaa9749abc94
praveengowd004 <142770647+praveengowd004@users.noreply.github.com>
2023-08-28 11:22:20 +0530

    Add files via upload

---
+++ Pattern_Programs/Number_Pattern_Program/Program8.java
@@ -0,0 +1,26 @@
+package number_pattern_programs;
+
+public class Program8 
+{
+
+	public static void main(String[] args) 
+	{
+		for(int i=1 ; i<=5 ; i++)
+		{
+			for(int j=1 ; j<=i ; j++)
+			{
+				if(i==j || i-j==2 || i==5&&j==1 )
+				{
+				System.out.print("1");
+				}
+				else 
+				{
+					System.out.print("0");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
