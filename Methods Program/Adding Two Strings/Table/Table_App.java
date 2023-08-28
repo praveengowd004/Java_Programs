0c472ab4bb6cbf6d77f3cb69fe66ee53b9f1766a
praveengowd004 <142770647+praveengowd004@users.noreply.github.com>
2023-08-28 09:13:08 +0530

    Add files via upload

---
+++ Methods Program/Adding Two Strings/Table/Table_App.java
@@ -0,0 +1,14 @@
+package table;
+
+public class Table_App 
+{
+	public static void table(int a)
+	{
+		for(int b=1;b<=10;b++)//checking condition
+		{
+			//printing table
+			System.out.println(a + " X "+ b + " = "+ (a*b));
+		}
+	}
+
+}
