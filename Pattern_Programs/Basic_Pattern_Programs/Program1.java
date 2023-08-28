071b1e9950e2c9872756fc7d6326d6a02f05d1af
praveengowd004 <142770647+praveengowd004@users.noreply.github.com>
2023-08-28 10:00:39 +0530

    Add files via upload

---
+++ Pattern_Programs/Basic_Pattern_Programs/Program1.java
@@ -0,0 +1,34 @@
+package Basic_Pattern_Programs;
+
+public class Program1 {
+
+	public static void main(String[] args)
+	{
+/*
+		for(int i=1;i<=5;i++)
+		{
+			System.out.print("*");
+			
+		}
+		
+		
+		//with while loop
+		int i=1;
+		while(i<=5)
+		{
+			System.out.print("*");
+			i++;
+		}
+*/		
+		
+		int i=1;
+		do
+		{
+			//printig star * in a horizontal line
+			System.out.print("*");
+			i++;
+		}
+		while(i<=5);
+	}
+
+}
