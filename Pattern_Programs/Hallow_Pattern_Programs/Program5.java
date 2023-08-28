b4ac21fb3b507307bc028aa07aaffbfe7c62ce90
praveengowd004 <142770647+praveengowd004@users.noreply.github.com>
2023-08-28 10:07:42 +0530

    Add files via upload

---
+++ Pattern_Programs/Hallow_Pattern_Programs/Program5.java
@@ -0,0 +1,32 @@
+package Hallow_Pattern_Programs;
+
+public class Program5 
+{
+	public static void main(String[] args) 
+	{
+
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(" ");
+			}
+			
+			for(int k=1;k<=(11-i-i);k++)
+			{
+		
+				if(i==1 || k==1 || (i==2&&k==7) || (i==3&&k==5) || (i==4&&k==3))
+				{
+				System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+				
+			}
+			System.out.println();
+		}
+	}
+
+}
