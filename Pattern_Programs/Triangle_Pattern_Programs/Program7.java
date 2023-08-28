80be249f9d550956df5d7ab1c60de87bda5d7add
praveengowd004 <142770647+praveengowd004@users.noreply.github.com>
2023-08-28 11:41:19 +0530

    Add files via upload

---
+++ Pattern_Programs/Triangle_Pattern_Programs/Program7.java
@@ -0,0 +1,23 @@
+package Triangle_Pattern_Programs;
+
+public class Program7 {
+
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				for(int k=1;k<=i;k++)
+				{
+				System.out.print("*");
+				}
+			
+			}
+			System.out.println();
+		}
+		
+		
+	}
+
+}
