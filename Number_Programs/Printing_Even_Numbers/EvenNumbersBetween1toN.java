24e5858e4a63253c58aab30ca286c819b69e2006
praveengowd004 <142770647+praveengowd004@users.noreply.github.com>
2023-08-28 17:23:32 +0530

    Add files via upload

---
+++ Number_Programs/Printing_Even_Numbers/EvenNumbersBetween1toN.java
@@ -0,0 +1,21 @@
+package EvenNumbers;
+
+public class EvenNumbersBetween1toN {
+
+	public static void even(int n)
+	{
+		//outer for loop
+		for(int i=1;i<=n;i++)
+		{
+			//inner for loop
+			if(i%2==0)
+			{
+				//printing i valve
+				System.out.println(i);
+				
+			}
+		}
+
+	}
+
+}
